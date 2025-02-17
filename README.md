43c4a8912d50e7f81e01da6ebae250357b547e25
Emil Barranco <emilbarranco@icloud.com>
2025-02-17 06:59:33 -0400

    Handling the CSV file pt. 1
    - Updated README file.
    - Created a class method to handle initializing and handling the CSV file.
    - Created a class method to add entries to the csv file.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Expense-Tracker-Pandas 💱📊
- Creating an expense tracker with python and pandas, a couple other python modules.
+ Creating an expense tracker with python and pandas, a couple other python modules. Following this **guided** [YouTube Project](https://youtu.be/Dn1EjhcQk64?si=RtCLu6AyAo0EWmFF).
 
 ## Overview
 TBD
--- core.py
+++ core.py
@@ -5,6 +5,7 @@ from datetime import datetime
 # Setting up a class that will contain the functions related to the CSV file.
 class CSV:
     CSV_FILE = 'finance_data.csv'
+    COLUMNS = ["date", "amount", "category", "description"]
 
     # This will have access to the class itself but it won't have access to its instance.
     @classmethod
@@ -13,4 +14,24 @@ class CSV:
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["date", "amount", "category", "description"])
+            df = pd.DataFrame(columns=cls.COLUMMS)
+            df.to_csv(cls.CSV_FILE, index=False)
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        # Storing the new data entry into a python dictionary when using the CSV Writer, to write into the correct columns.
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+        # Appending the entry to the end of the file.
+        with open(cls.CSV_FILE, "a", newline="") as csvfile: # context manager: handles closing the file, cleans up memory.
+            # This takes a dictionary and writes it into the specified csv file.
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+        print("Entry added successfully.")
+
+CSV.initialize_csv()
+CSV.add_entry("2025-01-01", 432.52, "Income", "Salary")
